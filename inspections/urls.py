a321d9dfdd36c38965d581b047631fc1dd28757d
Binitagarwal <rockingbinitagarwal@gamil.com>
2021-12-08 11:42:03 +0530

    views changes

--- inspections/urls.py
+++ inspections/urls.py
@@ -4,4 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('auth/login',loginView.as_view(),name='login'),
+    path('inspection/mystatus',mystatusView.as_view(),name='mystatus'),
+    path('inspection/myinspection',myinspectionView.as_view(),name='myinspection'),
+    path('inspection/myfieldReport',myfieldReportView.as_view(),name='myfieldReport'),
 ]
--- inspections/views.py
+++ inspections/views.py
@@ -17,9 +17,245 @@ class loginView(APIView):
         if(check_password(password,user.password)):
             response['token'] = user.token
             response['success'] = 'true'
-            print(response)
             return Response(response,status=200)
         else:
             response['status'] = 'fail'
             return Response("Invalid username or password",status=403)
+
+
+class mystatusView(APIView):
+    def get(self, request):
+        response = {}
+        print("GET under mystatus")
+        print(request.data)
+        #response['success'] = 'true'
+        response = {
+            'data':{
+                'success':'true',
+                'totalAssigned':'10',
+                'totalInspected':'5',
+                'totalClosed':'0',
+                'totalBypass':'4'
+            }
+        }
+
+        return Response(response,status=200)
+
+    def post(self, request):
+        response = {}
+        print("POST")
+        print(str(request))
+        print(request.data)
+        response['success'] = 'true'
+        # response['sector'] = 'heavy industry'
+        # response['unitcode'] = '1'
+        return Response(response,status=200)
+
+class myinspectionView(APIView):
+    def get(self, request):
+        response = {}
+        print("GET")
+        print(str(request))
+        print(request.data)
+        response['success'] = 'true'
+        response['sector'] = 'heavy industry'
+        response['state'] = 'West Bengal'
+        response['district'] = 'Kolkata'
+        response['basin'] = 'heavy industry'
+        response['unitcode'] = '1'
+
+        return Response(response,status=200)
+    def post(self, request):
+        response = {}
+        print("POST")
+        print(request.headers)
+        print(request.data)
+        # response = {
+        #     'data':{
+        #         'success':'true',
+        #         'factory':{
+        #             'unitcode':'1',
+        #             'name':'LNT',
+        #             'state':{'name':'West Bengal'},
+        #             'region':'kolkata area',
+        #             'district':{'name':'Kolkata'},
+        #             'basin':{'name':'heavy industry'},
+        #             'location':{'coordinates':'12.34,56.78'},
+        #         }
+        #     }
+        # }
+        response = [{
+            'success':'true',
+                "_id": "1",
+                "factory": {
+                    "location": {
+                        "coordinates": []
+                    },
+                    "_id": "1",
+                    "name": "N.I.F. PVT LTD (Namastey India Food), VILL-BRAHAMPUR SHIVRAJPUR, KANPUR",
+                    "sector": {
+                        "_id": "1",
+                        "name": "food & beverages"
+                    },
+                    "unitcode": 357,
+                    "state": {
+                        "_id": "1",
+                        "name": "Uttar Pradesh"
+                    },
+                    "district": {
+                        "_id": "1",
+                        "name": "Kanpur Nagar"
+                    },
+                    "region": "KANPUR NAGAR",
+                    "basin": {
+                        "_id": "1",
+                        "name": "ganga"
+                    }
+                },
+        },{
+            'success':'true',
+                "_id": "1",
+                "factory": {
+                    "location": {
+                        "coordinates": []
+                    },
+                    "_id": "1",
+                    "name": "N.I.F. PVT LTD (Namastey India Food), VILL-BRAHAMPUR SHIVRAJPUR, KANPUR",
+                    "sector": {
+                        "_id": "1",
+                        "name": "textile"
+                    },
+                    "unitcode": 357,
+                    "state": {
+                        "_id": "1",
+                        "name": "Uttar Pradesh"
+                    },
+                    "district": {
+                        "_id": "1",
+                        "name": "Kanpur Nagar"
+                    },
+                    "region": "KANPUR NAGAR",
+                    "basin": {
+                        "_id": "1",
+                        "name": "ganga"
+                    }
+                }
+        },{
+            'success':'true',
+            "_id": "1",
+                "factory": {
+                    "location": {
+                        "coordinates": []
+                    },
+                    "_id": "1",
+                    "name": "Complete",
+                    "sector": {
+                        "_id": "1",
+                        "name": "Nothing left"
+                    },
+                    "unitcode": 357,
+                    "state": {
+                        "_id": "1",
+                        "name": "Uttar Pradesh"
+                    },
+                    "district": {
+                        "_id": "1",
+                        "name": "Kanpur Nagar"
+                    },
+                    "region": "KANPUR NAGAR",
+                    "basin": {
+                        "_id": "1",
+                        "name": "ganga"
+                    }
+                }
+        }
+        ]
+        # response = {
+        #     'data':[{
+        #         'success':'true',
+        #         "_id": "1",
+        #         "factory": {
+        #             "location": {
+        #                 "coordinates": []
+        #             },
+        #             "_id": "1",
+        #             "name": "N.I.F. PVT LTD (Namastey India Food), VILL-BRAHAMPUR SHIVRAJPUR, KANPUR",
+        #             "sector": {
+        #                 "_id": "1",
+        #                 "name": "food & beverages"
+        #             },
+        #             "unitcode": 357,
+        #             "state": {
+        #                 "_id": "1",
+        #                 "name": "Uttar Pradesh"
+        #             },
+        #             "district": {
+        #                 "_id": "1",
+        #                 "name": "Kanpur Nagar"
+        #             },
+        #             "region": "KANPUR NAGAR",
+        #             "basin": {
+        #                 "_id": "1",
+        #                 "name": "ganga"
+        #             }
+        #         }
+        #     },{
+        #             'success':'true',
+        #         "_id": "1",
+        #         "factory": {
+        #             "location": {
+        #                 "coordinates": []
+        #             },
+        #             "_id": "1",
+        #             "name": "N.I.F. PVT LTD (Namastey India Food), VILL-BRAHAMPUR SHIVRAJPUR, KANPUR",
+        #             "sector": {
+        #                 "_id": "1",
+        #                 "name": "food & beverages"
+        #             },
+        #             "unitcode": 357,
+        #             "state": {
+        #                 "_id": "1",
+        #                 "name": "Uttar Pradesh"
+        #             },
+        #             "district": {
+        #                 "_id": "1",
+        #                 "name": "Kanpur Nagar"
+        #             },
+        #             "region": "KANPUR NAGAR",
+        #             "basin": {
+        #                 "_id": "1",
+        #                 "name": "ganga"
+        #             }
+        #         }
+        #     }]
+            
+        #}
+
+        #response = {'data':{'cool':'cool'}}
+
+        # response['sector'] = 'heavy industry'
+        # response['state'] = 'West Bengal'
+        # response['district'] = 'Kolkata'
+        # response['basin'] = 'heavy industry'
+        # response['unitcode'] = '1'
+
+        return Response(response,status=200)
+
+
+
+class myfieldReportView(APIView):
+    def post(self, request, *args, **kwargs):
+        response = {}
+        print("POST")
+        print(request.headers)
+        print('bhaiya maze',request.data)
+        response = {
+            'payload':{
+                'success':'true',
+            }
+        }
+        return Response(response,status=200)
+    def get(self, request):
+        return Response("cool",status=200)
+
     
