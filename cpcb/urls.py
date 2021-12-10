e41dc08eb93a588a680646376a5c190f72c7d0c1
Binitagarwal <rockingbinitagarwal@gamil.com>
2021-12-10 15:39:32 +0530

    debug mode off

--- cpcb/urls.py
+++ cpcb/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('api/',include('inspections.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
